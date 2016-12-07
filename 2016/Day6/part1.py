d174524cf56f028831e500b447a3d4dd4d622d47
Darius Ciupe <ciupedarius@gmail.com>
2016-12-07 14:40:52 +0200

    Day 6 solution.

---
+++ 2016/Day6/part1.py
@@ -0,0 +1,16 @@
+def main():
+    with open('input.txt') as file:
+        pointer = file.tell()  # save initial file pointer
+        size = len(file.readline().strip())
+        file.seek(pointer)  # go back to the pointer saved
+        col = ["" for x in range(size)]
+        for line in file.readlines():
+            for i, c in enumerate(line.strip()):
+                col[i] = col[i] + c
+        code = ""
+        for i in range(0, len(col)):
+            code = code + max([(j, col[i].count(j)) for j in set(col[i])], key=lambda x: x[1])[0]
+        print('Code: ', code)
+
+
+main()
