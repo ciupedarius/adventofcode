2400855e9341fabfd3d302b04dfe9a80365511eb
Darius Ciupe <darius.ciupe@recognos.ro>
2017-12-04 15:12:42 +0200

    Solution for day 3, part 2

--- 2017/Day3/day3.go
+++ 2017/Day3/day3.go
@@ -5,6 +5,11 @@ import (
 	"math"
 )
 
+type coord struct {
+	x int
+	y int
+}
+
 func main() {
 	input := 361527
 
@@ -43,5 +48,72 @@ func part1(input int) {
 }
 
 func part2(input int) {
+	// compute whole spiral..
+	num := 1        //current number
+	level := 2      // spiral level
+	count := 0      //numbers computed
+	side := "right" // current side
+	x := 1          //x coordinate
+	y := 0          //y coordinate
+	matrix := make(map[coord]int)
+	matrix[coord{0, 0}] = num
+	for input > num {
+		// compute number and switch side when reaching corners
+		switch side {
+		case "right":
+			num = sum_neighbor(matrix, x, y)
+			matrix[coord{x, y}] = num
+			count++
+			if count%level == 0 {
+				side = "up"
+				x--
+			} else {
+				y++
+			}
+		case "up":
+			num = sum_neighbor(matrix, x, y)
+			matrix[coord{x, y}] = num
+			count++
+			if count%level == 0 {
+				side = "left"
+				y--
+			} else {
+				x--
+			}
+		case "left":
+			num = sum_neighbor(matrix, x, y)
+			matrix[coord{x, y}] = num
+			count++
+			if count%level == 0 {
+				side = "down"
+				x++
+			} else {
+				y--
+			}
+		case "down":
+			num = sum_neighbor(matrix, x, y)
+			matrix[coord{x, y}] = num
+			count++
+			// when at right corner end, begin a new level
+			if count%level == 0 {
+				side = "right"
+				level += 2
+				x++
+			} else {
+				x++
+			}
+		}
+	}
+	fmt.Println("Number =", num)
+}
 
+// sum of all 8 matrix neighbors
+func sum_neighbor(m map[coord]int, x int, y int) int {
+	s := 0
+	for i := x - 1; i < x+2; i++ {
+		for j := y - 1; j < y+2; j++ {
+			s = m[coord{i, j}] + s
+		}
+	}
+	return s
 }
