b0b8f60f9d4c0e0a2e0a81563104f361b02e7d01
Darius Ciupe <darius.ciupe@recognos.ro>
2017-12-18 10:19:15 +0200

    Add solution for day 10 part 2

--- 2017/Day10/day10.go
+++ 2017/Day10/day10.go
@@ -3,41 +3,82 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strconv"
 	"strings"
 )
 
 func main() {
-	input, _ := ioutil.ReadFile("input.txt")
-	arr := make([]int, 0)
-	for _, s := range strings.Split(string(input), ",") {
-		val, _ := strconv.Atoi(s)
-		arr = append(arr, val)
+	var part int
+	// part is defined as cmd argument
+	if len(os.Args) > 1 && os.Args[1] == "part2" {
+		part = 2
+	} else {
+		//run part 1 as default
+		part = 1
 	}
+	input, _ := ioutil.ReadFile("input.txt")
+	array := make([]int, 0)
+	if part == 1 {
+		for _, s := range strings.Split(string(input), ",") {
+			val, _ := strconv.Atoi(s)
+			array = append(array, val)
+		}
+		list := make([]int, 256)
+		for i := range list {
+			list[i] = i
+		}
+		_, _, list = hashRound(array, 0, 0, list)
+		fmt.Println(list[0] * list[1])
+	} else {
+		part2(input)
+	}
+}
+
+func hashRound(array []int, position int, skip int, list []int) (int, int, []int) {
+	for _, v := range array {
+		// reverse elements
+		for k := 0; k < v/2; k++ {
+			i := (k + position) % len(list)
+			j := (v - 1 - k + position) % len(list)
+			list[i], list[j] = list[j], list[i]
+		}
+		position = (position + v + skip) % len(list)
+		skip++
+	}
+	return position, skip, list
+}
+
+func part2(input []byte) {
+	array := make([]int, 0)
+	sequence := []byte{17, 31, 73, 47, 23}
+	input = append(input, sequence...)
+	for _, v := range input {
+		array = append(array, int(v))
+	}
+	position := 0
+	skip := 0
 	list := make([]int, 256)
 	for i := 0; i < len(list); i++ {
 		list[i] = i
 	}
-	position := 0
-	skip := 0
-	for _, v := range arr {
-		list = reverse(list, position, v)
-		position = (position + v + skip) % len(list)
-		skip++
-
+	for i := 0; i < 64; i++ {
+		position, skip, list = hashRound(array, position, skip, list)
 	}
-	fmt.Println(list[0] * list[1])
-
-}
-
-func reverse(arr []int, position int, length int) []int {
-	arrLen := len(arr)
-	for k := 0; k < length/2; k++ {
-		i := (k + position) % arrLen
-		j := (length - 1 - k + position) % arrLen
-		temp := arr[i]
-		arr[i] = arr[j]
-		arr[j] = temp
+	denseHash := make([]int, 16)
+	for i := 0; i < 16; i++ {
+		for j := i * 16; j < (i+1)*16; j++ {
+			denseHash[i] = denseHash[i] ^ list[j]
+		}
 	}
-	return arr
+	hash := ""
+	for _, v := range denseHash {
+		hex := fmt.Sprintf("%x", v)
+		// add 0 padding to make a 2 digits number
+		if len(hex) == 1 {
+			hex = "0" + hex
+		}
+		hash = hash + hex
+	}
+	fmt.Println(hash)
 }
